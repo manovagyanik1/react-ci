37dd5fb81184c251e65b741ab1fe58c055647e43
apple <apple@apples-MacBook-Pro-2.local>
2020-03-18 12:21:43 +0000

    testing the CI

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello world, this is my sample react app
         </p>
         <a
           className="App-link"
